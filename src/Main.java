9ec33ba4d6bcf5d22444fc5684771bcf19971504
kriswang <zhaosen2@illinois.edu>
2018-09-18 11:53:43 -0500

    new project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    import java.io.IOException;
+    import java.net.URL;
+    import java.util.Scanner;
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/WebScraper.java
@@ -0,0 +1,19 @@
+public class WebScraper {
+    import java.io.IOException;
+    import java.net.URL;
+    import java.util.Scanner;
+    public static void main(String[] unused) {
+        public static String urlToString(final String url) {
+            Scanner urlScanner;
+            try {
+                urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+            } catch (IOException e) {
+                return "";
+            }
+            String contents = urlScanner.useDelimiter("\\A").next();
+            urlScanner.close();
+            return contents;
+        }
+
+    }
+}
